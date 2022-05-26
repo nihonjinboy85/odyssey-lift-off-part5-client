9a0b4c5d0c91a071f94e5d1e5e8d45774f250ad0
Richard Carrigan <me@richardcarrigan.com>
2022-05-25 23:46:45 -0700

    Change Apollo server URI to production Heroku API

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
+  uri: 'https://apollo-graphql-learning-api.herokuapp.com/',
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
